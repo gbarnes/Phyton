392acc41f0a2fc60a6a9ab3a5bb54e981e2f1eae
Gavin Barnes <gbarnes@cique.de>
2011-10-23 19:48:49 +0200

    implemented some dummy files and further structures

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Project 'Phyton'</title>
+</head>
+<body>
+    <p>
+        Initial html file please add some more code to the project
+    </p>
+</body>
+</html>
